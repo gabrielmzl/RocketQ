cb874932f3192b84d933d2c030a0b0db25790f22
gabrielmzl <mzlcs2k@gmail.com>
2021-06-28 00:28:49 -0300

    ✨ Fix a modal and add dynamic title

--- src/route.js
+++ src/route.js
@@ -2,8 +2,10 @@ const express = require('express')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index",))
-route.get('/room', (req, res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/', (req, res) => res.render("index", {title: "Início"}))
+route.get('/room', (req, res) => res.render("room", {title: "Perguntas e Respostas"}))
+route.get('/create-pass', (req, res) => res.render("create-pass", {title: "Criar Sala"}))
+
+route.post('/room/:room/:question/:action')
 
 module.exports = route
