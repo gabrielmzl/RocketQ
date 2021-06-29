99d36a9e7fddadfcb1494ecdf75cd138f57567b7
gabrielmzl <mzlcs2k@gmail.com>
2021-06-28 22:58:35 -0300

    🚀 finished the controllers and update readme

--- src/db/init.js
+++ src/db/init.js
@@ -4,15 +4,15 @@ const initDb = {
   async init() {
     const db = await Database()
     await db.exec(`CREATE TABLE rooms (
-      id INTEGER PRIMARY KEY,
-      pass TEXT
-    )`);
-
+            id INTEGER PRIMARY KEY,
+            pass TEXT
+        )`)
     await db.exec(`CREATE TABLE questions (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      titulo TEXT
-      read INT
-    )`);
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            read INT,
+            room INT
+        )`)
 
     await db.close()
   }
--- src/route.js
+++ src/route.js
@@ -7,8 +7,11 @@ const route = express.Router()
 route.get('/', (req, res) => res.render("index", {title: "Início", page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {title: "Criar Sala", page: 'create-pass'}))
 
-route.get('/room/:room', (req, res) => res.render("room", {title: "Perguntas e Respostas"}))
-route.post('/question/:room/:question/:action', QuestionController.index)
 route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+route.post('/enter-room', RoomController.enter)
+
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
 
 module.exports = route
