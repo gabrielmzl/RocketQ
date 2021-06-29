643a381eca0046aef351b1d7e6f7890f93070eba
gabrielmzl <mzlcs2k@gmail.com>
2021-06-28 21:42:39 -0300

    ✨ database connection initialized

--- src/route.js
+++ src/route.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index", {title: "Início"}))
-route.get('/room', (req, res) => res.render("room", {title: "Perguntas e Respostas"}))
-route.get('/create-pass', (req, res) => res.render("create-pass", {title: "Criar Sala"}))
+route.get('/', (req, res) => res.render("index", {title: "Início", page: 'enter-room'}))
+route.get('/create-pass', (req, res) => res.render("index", {title: "Criar Sala", page: 'create-pass'}))
 
-route.post('/room/:room/:question/:action')
+route.get('/room/:room', (req, res) => res.render("room", {title: "Perguntas e Respostas"}))
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create)
 
 module.exports = route
--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ server.use(express.static("public"));
 server.set('view engine', 'ejs');
 server.set('views', path.join(__dirname, 'views'));
 
+server.use(express.urlencoded({extended: true}))
 server.use(route)
 
 server.listen(3000, () => console.log(`Server initialized in port 3000.`))
